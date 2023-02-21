19cbb2eec4ce30bebc215a8022e6ebf219c70f64
unknown <emprenderusinque@hotmail.com>
2023-02-21 14:47:51 -0500

    Breakingbad Component

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
+import { BreackingBadApp } from './src/breakingbad/breakingbad-app'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -9,11 +10,15 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1 id="app-tittle">Hello Vite!</h1>
+    <h1 id="app-title">Hello Vite!</h1>
     <div class="card">
 
     </div>
 
   </div>
 `
+const element = document.querySelector('.card')
+
+
+BreackingBadApp( element );
 
