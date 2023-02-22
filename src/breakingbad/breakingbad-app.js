020e1bf67bbdde769b8f2911f90b72f9a91a3f29
unknown <emprenderusinque@hotmail.com>
2023-02-21 21:23:36 -0500

    mostrar quote en pantalla

--- src/breakingbad/breakingbad-app.js
+++ src/breakingbad/breakingbad-app.js
@@ -1,10 +1,31 @@
+
+/**
+ * @returns {Promise<Object> } quote information
+ */
+
+const fetchQuote = async() => {
+
+    const resp = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const data = await resp.json();
+
+    console.log(data[0])
+    return data;
+
+}
+
 /**
  * 
  * @param {HTMLDivElement} element 
  */
 
-export const BreackingBadApp = ( element ) => {
-    document.querySelector('#app-title').innerHTML = 'Breakingbad App';
-    element.innerHTML = 'Loading...'
+export const BreackingBadApp = async( element ) => {
 
+    document.querySelector('#app-title').innerHTML = 'Breakingbad App';
+    element.innerHTML = 'Loading...';
+
+    const quote = await fetchQuote();
+
+    element.innerHTML = 'tenemos data'
+
+    
 }
